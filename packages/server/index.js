1ecac885258ef7d27d608aec5449e348cfbc0803
Kangyun Wang <kevinwang8011@hotmail.com>
2019-04-19 18:36:44 +0200

    Base setup server

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,1 +1,10 @@
-console.log('SERVER TEST');
+const express = require('express');
+const app = express();
+
+const port = 4000;
+
+app.get('/', (req, res) => {
+	res.send('TEST');
+});
+
+app.listen(port, console.log(`Listening to port: ${port}`));
